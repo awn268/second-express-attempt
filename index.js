3cb8ebf33273e193a6bc20bd451000950f6ae8ad
Austin Nichols <austinnichols@Austins-Mac-mini.local>
2026-06-07 14:07:35 -0500

    Add postgres functionality

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 import express from "express";
+import pg from "pg";
+import dotenv from "dotenv";
+import db from "./db.js";
 
 const app = express();
 const port = process.env.PORT;
+
+let posta_data = [];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+db.connect();
+
+db.query("SELECT * FROM posta", (err, res) => {
+    if (err) {
+        console.error("Error executing query", err.stack);
+    } else {
+        posta_data = res.rows;
+    }
+
+    db.end();
+});
 
 app.use(express.static("public"));
 
@@ -14,7 +34,7 @@ app.get("/piping", (req, res) => {
 });
 
 app.get("/reading", (req, res) => {
-    res.render("reading.ejs");
+    res.render("reading.ejs", {data: posta_data});
 });
 
 app.get("/writing", (req, res) => {
