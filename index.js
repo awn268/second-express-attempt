4b16f27335e307efe2348132f45dfdd3b00d7eb2
Austin Nichols <austinnichols@Austins-Mac-mini.local>
2026-06-13 21:16:49 -0500

    Add post functionality

--- index.js
+++ index.js
@@ -33,6 +33,19 @@ app.get("/writing", (req, res) => {
     res.render("writing.ejs");
 });
 
+app.get("/posts/:slug", async (req, res) => {
+    try {
+        const result = await db.query("SELECT * FROM posta WHERE slug = $1",
+            [req.body.slug]
+        );
+        res.render("post.ejs", 
+            { data: result.rows }
+        );
+    } catch (err) {
+        console.error("Error executing query", err.stack);
+        res.status(500).send("Database query failed");
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
