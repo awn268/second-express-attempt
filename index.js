37a8195be8b9d7968b56af0c2dc9a0e534a75bd2
Austin Nichols <austinnichols@Austins-Mac-mini.local>
2026-06-28 13:01:25 -0500

    Fix port variable bug

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import db from "./db.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.LOCAL_PORT;
+const port = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
