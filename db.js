17b46d0647129171e4604f92b1079fe20453b93e
Austin Nichols <austinnichols@Austins-Mac-mini.local>
2026-08-09 19:47:13 -0500

    Prepare database connection for push

--- db.js
+++ db.js
@@ -5,16 +5,16 @@ dotenv.config();
 
 const { Pool } = pg;
 
-// const db = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-// });
-
 const db = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+    connectionString: process.env.DATABASE_URL,
 });
+
+// const db = new Pool({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   port: process.env.PG_PORT,
+// });
 
 export default db;
